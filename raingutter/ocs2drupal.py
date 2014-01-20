6dd424d1910852c308cdc646bd0cd7f6798d3966
dmalament <dmalament@940247f8-a3eb-dc11-81c7-0019d144f6e4>
2014-01-20 19:41:22 +0000

    add on-spec imports to ocs2drupal
    git-svn-id: svn://fountainhead.talkpoint.com/raingutter/trunk@12827 940247f8-a3eb-dc11-81c7-0019d144f6e4

--- raingutter/ocs2drupal.py
+++ raingutter/ocs2drupal.py
@@ -14,6 +14,12 @@ site I designed.
 #                               IMPORTS
 ########################################################################
 
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+
+from pprint import pprint as pp  # for debugging
+
 import math
 
 
